5cab3eb20e7c42969cbd023dc3023d679f4f0b00
Archie Fan <archiefan@gmail.com>
2022-10-26 16:09:45 +0100

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,43 @@
+﻿using DemoLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPerson owner = Factory.CreatePerson();
+            owner.FirstName = "Archie";
+            owner.LastName = "Fan";
+            owner.EmailAddress = "archiefan@gmail.com";
+            owner.PhoneNumber = "07621187088";
+            //Person owner = new Person
+            //{
+            //    FirstName = "Archie",
+            //    LastName = "Fan",
+            //    EmailAddress = "archiefan@gmail.com",
+            //    PhoneNumber = "07621187088"
+            //};
+
+            IChore chore = Factory.CreateChore();
+            chore.ChoreName = "Take out the trash";
+            chore.Owner = owner;
+            //Chore chore = new Chore
+            //{
+            //    ChoreName = "Take out the trash",
+            //    Owner = owner
+            //};
+
+            chore.PerformedWork(3);
+            chore.PerformedWork(1.5);
+            chore.CompleteChore();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DemoLibrary/Chore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoLibrary
+{
+    public class Chore : IChore
+    {
+        public string ChoreName { get; set; }
+        public IPerson Owner { get; set; }
+        public double HoursWorked { get; private set; }
+        public bool IsComplete { get; private set; }
+
+        private readonly ILogger _logger;
+        private readonly IMessageSender _messageSender;
+        public Chore(ILogger logger, IMessageSender messageSender)
+        {
+            _logger = logger;
+            _messageSender = messageSender;
+        }
+        public void PerformedWork(double hours)
+        {
+            HoursWorked += hours;
+            //Logger log = new Logger();
+            _logger.Log($"Performed work on {ChoreName}");
+        }
+        public void CompleteChore()
+        {
+            IsComplete = true;
+
+            //Logger log = new Logger();
+            _logger.Log($"Completed {ChoreName}");
+
+            //Emailer emailer = new Emailer();
+            _messageSender.SendMessage(Owner, $"The chore {ChoreName} is complete.");
+        }
+    }
+}
